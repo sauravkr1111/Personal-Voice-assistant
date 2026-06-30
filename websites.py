b78982bf0f5061e66e61fb7d1af17996a7e41c55
Saurav Kumar <ssauravkr1111@gmail.com>
2026-06-30 11:05:21 +0530

    Some frequently used websites

---
+++ websites.py
@@ -0,0 +1,13 @@
+websites = {
+    "amazon": "https://www.amazon.in",
+    "telegram": "https://web.telegram.org",
+    "youtube": "https://www.youtube.com",
+    "google": "https://www.google.com",
+    "gmail": "https://mail.google.com",
+    "chatgpt": "https://chatgpt.com",
+    "whatsapp": "https://web.whatsapp.com",
+    "instagram": "https://www.instagram.com",
+    "facebook": "https://www.facebook.com",
+    "linkedin": "https://www.linkedin.com",
+    "github": "https://github.com",
+}
